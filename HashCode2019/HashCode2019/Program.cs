7e290bc6dff6138606afac5784b1dac832a3d0f9
Adán <adan@shellkron.com>
2019-01-17 00:29:18 +0100

    Se añade configuración básica

---
+++ HashCode2019/HashCode2019/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HashCode2019
+{
+    class Program
+    {
+        public static Settings Configuracion { get; set; } = new Settings();
+
+        static void Main(string[] args)
+        {
+            Setup(args);
+
+        }
+
+        private static void Setup(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables("HashCode2019_")
+            .AddCommandLine(args);
+
+            builder.Build().Bind(nameof(Settings), Configuracion);
+        }
+    }
+}
---
+++ HashCode2019/HashCode2019/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2019
+{
+    public class Settings
+    {
+        private string _dataDirectory = string.Empty;
+        public string DataDirectory
+        {
+            get
+            {
+                return _dataDirectory;
+            }
+            set
+            {
+                if (!value.StartsWith("~/") && !System.IO.Directory.Exists(value))
+                    throw new Exception("La ruta del directorio no existe. Solo rutas absolutas o relativas mediante la convención '~/'.");
+
+                _dataDirectory = value.Replace('/', System.IO.Path.DirectorySeparatorChar).Replace('\\', System.IO.Path.DirectorySeparatorChar).Replace($"~{System.IO.Path.DirectorySeparatorChar}", $"{System.IO.Directory.GetCurrentDirectory()}{System.IO.Path.DirectorySeparatorChar}");
+                if (!System.IO.Directory.Exists(_dataDirectory))
+                    throw new Exception($"La ruta del directorio '{_dataDirectory}' no existe.");
+            }
+        }
+
+        private string _output = string.Empty;
+        public string OutputDirectory
+        {
+            get
+            {
+                return _output;
+            }
+            set
+            {
+                _output = value.Replace('/', System.IO.Path.DirectorySeparatorChar).Replace('\\', System.IO.Path.DirectorySeparatorChar).Replace($"~{System.IO.Path.DirectorySeparatorChar}", $"{System.IO.Directory.GetCurrentDirectory()}{System.IO.Path.DirectorySeparatorChar}");
+                if (!System.IO.Directory.Exists(_output))
+                    System.IO.Directory.CreateDirectory(_output);
+            }
+        }
+
+        public List<string> FilesName { get; set; }
+    }
+}
