05dd2356bd17c69aadb3df5ca9928999865a2a25
Adán <adan@shellkron.com>
2019-02-28 19:57:35 +0100

    Se añade clase slide

---
+++ HashCode2019/HashCode2019/Model/Slide.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HashCode2019
+{
+    public class Slide
+    {
+        public List<Photo> Photos;
+
+        public List<string> Tags
+        {
+            get
+            {
+                return new List<string>(); //Photos.SelectMany(p => p.Tags).GroupBy((s, k) => s);
+            }
+        }
+
+        public Slide(List<Photo> photos)
+        {
+            Photos = photos;
+        }
+
+    }
+}
