8ca4a5f6c6c264d7d329b6e34ffdd54e0c2505cd
Alberto Carbonell Rubio <cr.alberto2@gmail.com>
2020-02-20 18:51:05 +0100

    Added first empty structure + files

---
+++ HashCode2020/NoTePreocupes/HashCode2020/InputReader.cs
@@ -0,0 +1,29 @@
+﻿using HashCode2020.Models;
+
+namespace HashCode2020
+{
+    public class InputReader
+    {
+        private readonly string _filePath;
+
+        public InputReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public Input GetInput()
+        {
+            //var file = new StreamReader(_filePath);
+            //var line1 = file.ReadLine();
+            //int m = int.Parse(line1.Split(' ')[0]);
+            //int n = int.Parse(line1.Split(' ')[1]);
+            //var line2 = file.ReadLine();
+            //var pizzaSlices = line2.Split(' ').Select(int.Parse).ToArray();
+            //file.Dispose();
+            return new Input();
+
+        }
+
+
+    }
+}
---
+++ HashCode2020/NoTePreocupes/HashCode2020/Program.cs
@@ -0,0 +1,20 @@
+﻿using HashCode2020;
+using HashCode2020.Models;
+
+namespace PracticeRound2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read input from args
+            InputReader inputReader = new InputReader(args[0]);
+            Input input = inputReader.GetInput();
+
+            // Do the work
+
+
+            // Place output somewhere
+        }
+    }
+}
